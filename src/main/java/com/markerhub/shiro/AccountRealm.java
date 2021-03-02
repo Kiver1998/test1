1ef23319e7f86ceb449bfa22f28e4097a243f0d4
kiver <664703064@qq.com>
2021-03-02 12:42:00 +0800

    shiro

--- src/main/java/com/markerhub/shiro/AccountRealm.java
+++ src/main/java/com/markerhub/shiro/AccountRealm.java
@@ -1,12 +1,16 @@
 package com.markerhub.shiro;
 
-import org.apache.shiro.authc.AuthenticationException;
-import org.apache.shiro.authc.AuthenticationInfo;
-import org.apache.shiro.authc.AuthenticationToken;
+import cn.hutool.core.bean.BeanUtil;
+import com.markerhub.entity.User;
+import com.markerhub.service.UserService;
+import com.markerhub.util.JwtUtils;
+import org.apache.shiro.authc.*;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.authz.permission.PermissionResolver;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 
@@ -15,6 +19,12 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class AccountRealm extends AuthorizingRealm {
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Autowired
+    UserService userService;
+
 
     @Override
     public boolean supports(AuthenticationToken token){
@@ -29,7 +39,19 @@ public class AccountRealm extends AuthorizingRealm {
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
      JwtToken jwtToken=(JwtToken) token;
-        System.out.println("");
-        return null;
+     String userId=jwtUtils.getClaimByToken((String)jwtToken.getPrincipal()).getSubject();
+     User user=userService.getById(Long.valueOf(userId));
+     if(user==null){
+         throw new UnknownAccountException("账户不存在");
+
+     }
+     if (user.getStatus()==-1){
+         throw new LockedAccountException("账户已被锁定");
+
+     }
+     AccountProfile profile=new AccountProfile();
+        BeanUtil.copyProperties(user,profile);
+
+        return new SimpleAuthenticationInfo(profile,jwtToken.getCredentials(),getName());
     }
 }
--- src/main/java/com/markerhub/shiro/JwtFileter.java
+++ src/main/java/com/markerhub/shiro/JwtFileter.java
@@ -1,23 +1,34 @@
 package com.markerhub.shiro;
 
 import cn.hutool.http.server.HttpServerRequest;
+import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.markerhub.common.lang.Result;
+import com.markerhub.util.JwtUtils;
+import io.jsonwebtoken.Claims;
+import org.apache.catalina.filters.ExpiresFilter;
+import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.ExpiredCredentialsException;
 import org.apache.shiro.web.filter.authc.AuthenticatingFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author yan
  */
 @Component
 public class JwtFileter extends AuthenticatingFilter {
-    @Override
+    JwtUtils jwtUtils;
+   @Override
     protected AuthenticationToken createToken(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
-        HttpServerRequest request=(HttpServerRequest) servletRequest;
+       HttpServletRequest request=(HttpServletRequest) servletRequest;
         String jwt =request.getHeader("Authorization");
         if (StringUtils.isEmpty(jwt)){
             return null;
@@ -33,9 +44,31 @@ public class JwtFileter extends AuthenticatingFilter {
             return true;
         }else {
             //校验jwt
+
+         Claims claims=jwtUtils.getClaimByToken(jwt);
+         if(claims ==null||jwtUtils.isTokenExpired(claims.getExpiration())){
+             throw new ExpiredCredentialsException("token已失效，请重新登录");
+
+         }
             //执行登录
+            return  executeLogin(servletRequest,servletResponse);
         }
 
-        return false;
+
+    }
+
+    @Override
+    protected boolean onLoginFailure(AuthenticationToken token, AuthenticationException e, ServletRequest request, ServletResponse response) {
+       HttpServletResponse httpServletResponse=(HttpServletResponse) response;
+       Throwable throwable = e.getCause() == null ? e :e.getCause();
+        Result result =Result.fail(throwable.getMessage());
+        String json = JSONUtil.toJsonStr(result);
+
+        try{
+            httpServletResponse.getWriter().print(json);
+        }catch (IOException ioException){
+
+        }
+       return false;
     }
 }
